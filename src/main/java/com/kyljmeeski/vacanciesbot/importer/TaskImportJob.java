72a3d2f1ae4ce085e1f29b212cb44fa355d63498
kyljmeeski <kyljmeeski@gmail.com>
2024-05-14 19:07:36 +0600

    feat: iterate over pages retrieve vacancies from every of them

--- src/main/java/com/kyljmeeski/vacanciesbot/importer/TaskImportJob.java
+++ src/main/java/com/kyljmeeski/vacanciesbot/importer/TaskImportJob.java
@@ -1,12 +1,39 @@
 package com.kyljmeeski.vacanciesbot.importer;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.kyljmeeski.plainhttps.BodyWrapper;
+import com.kyljmeeski.plainhttps.MissingFieldException;
+import com.kyljmeeski.plainhttps.Request;
+import com.kyljmeeski.plainhttps.Response;
+import com.kyljmeeski.plainhttps.request.GetRequest;
+
 import java.util.function.Consumer;
 
 public class TaskImportJob implements Consumer<String> {
 
     @Override
     public void accept(String message) {
-        System.out.println(message);
+        TaskMessage taskMessage = new TaskMessage(message);
+        int pages = taskMessage.pages();
+        for (int i = 1; i <= pages; i++) {
+            Request request = new GetRequest("devkg.com/api/pages/jobs?page=" + i);
+            Response response = request.execute();
+            if (response.status().code() == 200) {
+                try {
+                    JsonObject json = new BodyWrapper(response.body()).content(JsonObject.class);
+                    JsonArray vacancies = json.getAsJsonObject("result").getAsJsonArray("list");
+                    for (JsonElement element : vacancies) {
+                        String slug = element.getAsJsonObject().get("slug").getAsString();
+                        Vacancy vacancy = new Vacancy(slug);
+                    }
+
+                } catch (MissingFieldException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     @Override
